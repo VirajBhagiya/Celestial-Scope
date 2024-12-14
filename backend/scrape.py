1b4710732944f23c914dab0f5b4b42b897052c2f
Viraj Bhagiya <virajbhagiya@gmail.com>
2024-12-14 21:37:07 +0530

    inital commit

--- backend/scrape.py
+++ backend/scrape.py
@@ -25,17 +25,18 @@ def get_celestial_coordinates(celestial_name):
     # Search for the celestial body
     search_box.clear()
     search_box.send_keys(celestial_name)
-    time.sleep(2)
+    time.sleep(3)
     search_box.send_keys(Keys.TAB)
     search_box.send_keys(Keys.ENTER)
-    time.sleep(2)
+    search_box.send_keys(Keys.RETURN)
+    
     
     
     ra_dec_div = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, 'radecVal'))
+        EC.presence_of_element_located((By.XPATH, '//*[@id="stel"]/div/div/div[10]/div[3]/div[4]/div[2]'))
     )
     
-    ra_dec_values = ra_dec_div.find_elements(By.CLASS_NAME, 'radecVal')
+    ra_dec_values = ra_dec_div.find_elements(By.XPATH, '/html/body/div[2]/div/div[1]/main/div/div/div/div/div/div[10]/div[3]/div[4]/div[2]')
     
     print(ra_dec_values)
     
